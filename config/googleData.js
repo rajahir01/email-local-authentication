3dbdd33b2cf422576ac114aea12ff0d3ea351c01
raj ahirwar r <118012012592@pmu.edu>
2022-07-01 22:51:43 +0530

    first commit

---
+++ config/googleData.js
@@ -0,0 +1,5 @@
+module.exports.clientId = "403021542856-6d8d7n7a59n5viq070anld0sq5vpkapn.apps.googleusercontent.com";
+module.exports.clientSecret = "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";
+
+
+// {"web":{"client_id":"403021542856-6d8d7n7a59n5viq070anld0sq5vpkapn.apps.googleusercontent.com","project_id":"vital-reef-351312","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:8000/google/callback/"]}}
---
+++ config/monkoKEY.js
@@ -0,0 +1,4 @@
+module.exports = "mongodb+srv://rajahirwar:1RsGdX4H1vuwYIX2@cluster0.mdgep.mongodb.net/?retryWrites=true&w=majority";
+
+
+// paswrd= 8GTv7CNgJp8MBNNB
